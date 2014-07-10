d0a46fb8e29ec7115f987792bc530884ab33cd62
AndyRBaker <Bakertyper@gmail.com>
2014-07-10 18:01:33 -0500

    I played with boxes

---
+++ public/master.js
@@ -0,0 +1,1 @@
+!function(){console.log("%c:: hello world ::","background:#333;color:#fff")}();
---
+++ public/scripts/dist/compiled.js
@@ -0,0 +1,4 @@
+(function myFancyFunctionName(){
+	// alert('sign up for rewards and stuff');
+	console.log('%c:: hello world ::','background:#333;color:#fff');
+})();
