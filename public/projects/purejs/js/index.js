2391b3c4a4368874e210ec6d7deb21cede24c244
AndyRBaker <Bakertyper@gmail.com>
2015-04-13 10:37:45 -0500

    Update 0.1.13
    Updated:
    
    - folder structure ( moved all my projects into a new folder called
    projects)
    - index menu to new logo and copy styles
    - removed copy
    - updated the links for the projects to the new folder system
    - updated new styles
    - updated the bike outlook to newest functioning version

---
+++ public/projects/purejs/js/index.js
@@ -0,0 +1,12 @@
+var firstDiv = document.getElementById('one');
+console.log(firstDiv) 
+
+function myFunction() {
+    document.getElementById("one").innerHTML = "1";
+}
+function dasFunction() {
+    document.getElementById("two").innerHTML = "2";
+}
+function meinFunction() {
+    document.getElementById("three").innerHTML = "3";
+}
