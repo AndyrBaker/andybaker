dcebf58ee65dcc29af254383430cf65b515ef3b0
AndyRBaker <Bakertyper@gmail.com>
2015-12-14 16:25:53 -0600

    updated package and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,47 @@
+
 //	include gulp
+
 	var gulp = require('gulp');
 
 //	include plugins
 
-	//	livereload
-	livereload = require('gulp-livereload');
+	//	global
+
+		var connect = require('gulp-connect');
+		var livereload = require('gulp-livereload');
+		var rename = require('gulp-rename');
 
 	//	sass
-	var sass = require('gulp-sass');
+
+		var sass = require('gulp-sass');
 
 	//	css
-	var minifyCSS = require('gulp-minify-css');
+
+		var minifyCSS = require('gulp-minify-css');
 
 	//	js
-	var jshint = require('gulp-jshint');
-	var concat = require('gulp-concat');
-	var uglify = require('gulp-uglify');
 
-	//	global
-	var rename = require('gulp-rename');
-	var connect = require('gulp-connect');
+		var jshint = require('gulp-jshint');
+		var concat = require('gulp-concat');
+		var uglify = require('gulp-uglify');
 
 //	specific tasks
 
-	//	pages
-
-		// gulp.task('homepage', function() {
-		// 	gulp.src('public/styles/*.scss')
-		// 		.pipe(sass())
-		// 		.pipe(minifyCSS({keepBreaks:true}))
-		// 		.pipe(gulp.dest('public/styles/dist/'))
-		// 		.pipe(livereload());
-		// 	gulp.src('public/scripts/*.js')
-		// 		.pipe(jshint())
-		// 		.pipe(jshint.reporter('default'))
-		// 		.pipe(uglify())
-		// 		.pipe(gulp.dest('public/scripts/dist/'))
-		// 		.pipe(livereload());
-		// });
+	//	homepage
 
 		gulp.task('homepage', function() {
 			gulp.src('public/*.html')
+				.pipe(livereload());
+			gulp.src('public/styles/*.scss')
+				.pipe(sass())
+				.pipe(minifyCSS({keepBreaks:true}))
+				.pipe(gulp.dest('public/styles/dist/'))
+				.pipe(livereload());
+			gulp.src('public/scripts/*.js')
+				.pipe(jshint())
+				.pipe(jshint.reporter('default'))
+				.pipe(uglify())
+				.pipe(gulp.dest('public/scripts/dist/'))
 				.pipe(livereload());
 		});
 
@@ -58,18 +59,29 @@
 
 		livereload.listen();
 
-		//	/index.html
-		gulp.watch(['public/*.html'], ['homepage']);
-		// gulp.watch(['public/*.html'], ['homepage']);
-		// gulp.watch(['public/scripts/*.js'], ['homepage']);
-		// gulp.watch(['public/styles/*.scss'], ['homepage']);
+		//	homepage
+
+			gulp.watch([
+				'public/*.html',
+				'public/styles/*.scss',
+				'public/scripts/*.js'
+			], ['homepage']);
 
 	});
 
-//	build tasks
+//	specific tasks
 
-	gulp.task('rebuild', ['homepage', 'watch', 'connect']);
+	gulp.task('build', [
+		//	homepage
+			'homepage',
+		//	global
+			'watch',
+			'connect'
+	]);
 
 //	default task
 
-	gulp.task('default', ['watch', 'connect']);
+	gulp.task('default', [
+		'watch',
+		'connect'
+	]);
